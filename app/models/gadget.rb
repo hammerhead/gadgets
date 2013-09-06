016d55dbd10b424457d09505f8f65141c0d69a3c
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-09-06 19:28:42 +0200

    Add validations for name and brand to model class

--- app/models/gadget.rb
+++ app/models/gadget.rb
@@ -1,3 +1,8 @@
 class Gadget < ActiveRecord::Base
+  include ActiveModel::Validations
+
+  validates :name, presence: true
+  validates :brand, presence: true
+
   attr_accessible :name, :brand, :buy_date
 end
