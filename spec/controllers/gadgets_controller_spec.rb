0af3cb6e9ec3c8c7b36a6c67ed744a2b6594d97e
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-09-06 17:58:42 +0200

    Adapt GadgetsControllerSpec to respect authentication

--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -1,7 +1,26 @@
 require 'spec_helper'
 
 describe GadgetsController do
-  context '#show' do
+  # TODO verify that the 302 redirect is actually the login page
+  context 'unauthenticated' do
+    it 'restricts access to #index' do
+      get :index
+
+      expect(response.code.to_i).to eq(302)
+    end
+
+    it 'restricts access to #show' do
+      get :show, gadget_id: 1
+
+      expect(response.code.to_i).to eq(302)
+    end
+  end
+
+  context 'authenticated' do
+    before(:each) do
+      sign_in FactoryGirl.create(:user)
+    end
+
     let(:gadget) { double('gadget', id: 1) }
 
     it 'returns all Gadgets' do
