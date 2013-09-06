31a7343a7c895e87170b386ad67542ae53398a31
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-09-06 17:27:36 +0200

    Have an index method returning all gadgets

--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -4,6 +4,14 @@ describe GadgetsController do
   context '#show' do
     let(:gadget) { double('gadget', id: 1) }
 
+    it 'returns all Gadgets' do
+      Gadget.should_receive(:all)
+
+      get :index
+
+      expect(response).to be_ok
+    end
+
     it 'returns a Gadget object' do
       Gadget.should_receive(:find).with(gadget.id.to_s).and_return(gadget)
 
