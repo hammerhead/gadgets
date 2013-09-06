4ae237a8e0acf81b2e650813a1a658984667896b
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-09-06 17:24:52 +0200

    Ensure a gadget is fetched

--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -1,8 +1,12 @@
+require 'spec_helper'
+
 describe GadgetsController do
   context '#show' do
     let(:gadget) { double('gadget', id: 1) }
 
     it 'returns a Gadget object' do
+      Gadget.should_receive(:find).with(gadget.id.to_s).and_return(gadget)
+
       get :show, gadget_id: gadget.id
 
       expect(response).to be_ok
