9de9ab68da8e135efd9a063370a4733a20438897
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-09-06 19:28:24 +0200

    Require a valid name and brand for Gadget

--- spec/models/gadget_spec.rb
+++ spec/models/gadget_spec.rb
@@ -4,4 +4,14 @@ describe Gadget do
   it { should allow_mass_assignment_of(:name) }
   it { should allow_mass_assignment_of(:brand) }
   it { should allow_mass_assignment_of(:buy_date) }
+
+  context 'validations' do
+    it 'requires a name' do
+      expect(Gadget.new(brand: 'cool brand')).to have(1).errors_on(:name)
+    end
+
+    it 'requires a brand' do
+      expect(Gadget.new(name: 'my gadget')).to have(1).errors_on(:brand)
+    end
+  end
 end
