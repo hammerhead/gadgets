8b9ee18c568686e23c9ccc56827aeb1edef928b8
Niklas Schmidtmer <nschmidtmer@crealytics.de>
2013-09-06 17:20:19 +0200

    Add route for showing a gadget

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Gadgets::Application.routes.draw do
+  resource :gadgets, only: [:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
